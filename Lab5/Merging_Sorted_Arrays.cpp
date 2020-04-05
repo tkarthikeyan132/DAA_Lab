f31402902e0d35f41ef0b04dd3c86f639b5dfe98
Animesh241100 <60282715+Animesh241100@users.noreply.github.com>
2020-04-05 14:48:34 +0530

    The final file for this

--- Lab5/Merging_Sorted_Arrays.cpp
+++ Lab5/Merging_Sorted_Arrays.cpp
@@ -11,7 +11,7 @@ class Unit
         int size;    //size of the array
         int arr[50]; //the array of sorted integers with default size 50.
         int pointer; //pointer to the first index which produces.
-        void initializer();
+        Unit();
 };
 
 class Pair
@@ -24,15 +24,16 @@ class Pair
             Array_id = id; 
             value = val; 
         } 
+        
         int getARR() const { return Array_id; } 
-        int getVAL() const { return value; } 
+        int getVAL() const { return value; }  
 }; 
   
 // To compare two Pairs 
 class myComparator 
 { 
     public: 
-        int operator() (const Pair& p1, const Pair& p2) 
+        int operator() (const Pair& p1, const Pair& p2)
         { 
             return p1.getVAL() > p2.getVAL(); 
         } 
@@ -57,11 +58,13 @@ int main()
     }
     vector<int> Sorted_Array(Final_Array_Size);
     Sorted_Array = Sorter(Unit_Array, k, Final_Array_Size);
+    cout << "The sorted Array is:\n";
     for(int i = 0; i < Final_Array_Size; i++)
     {
         cout << " " << Sorted_Array[i];
     }
-    return 0;
+    cout << endl;
+    return 0;   
 }
 
 vector<int> Sorter(Unit Unit_Array[], int k, int Final_Array_Size)
@@ -72,12 +75,30 @@ vector<int> Sorter(Unit Unit_Array[], int k, int Final_Array_Size)
     for(int i = 0; i < k; i++)
     {
         Heap.push(Pair(i, Unit_Array[i].arr[Unit_Array[i].pointer]));
-    }     
-
-
+    } 
+    
+    while(j < Final_Array_Size)
+    {
+        Pair P = Heap.top(); 
+        Heap.pop();
+        Final_vector[j] = P.getVAL();
+        j++; 
+        if(Unit_Array[P.getARR()].pointer < Unit_Array[P.getARR()].size)
+        {
+            Unit_Array[P.getARR()].pointer = Unit_Array[P.getARR()].pointer + 1;
+            P.Array_id = P.getARR();
+            if(Unit_Array[P.getARR()].pointer < Unit_Array[P.getARR()].size)
+            {
+                P.value = Unit_Array[P.getARR()].arr[Unit_Array[P.getARR()].pointer];
+                Heap.push(P);
+            }
+        } 
+    }
+    return Final_vector;    
 }
 
-void Unit::initializer()
+Unit::Unit()
 {
     pointer = 0;
 }
+
