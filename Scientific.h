932a2d0708b432d85f2e0db4d6f5cdd590659809
T Karthikeyan <49520037+tkarthikeyan132@users.noreply.github.com>
2020-04-08 21:28:00 +0530

    Delete Scientific.h

--- Scientific.h
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include "Basic.h"
-
-double Sin(float x);
-double Cos(float x);
-double Random(int Max);
-double Binomial(double x);
-double Poisson_pdf(int n, double parameter);
-float Std_deviation(float * arr, int size);
